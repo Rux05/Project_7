89547d9a67e80aa2cc48237691555eb44d43aece
Rux05 <rux05@users.noreply.github.com>
2024-03-27 13:00:28 +0400

    update modal

--- FrontEnd/assets/js/admin.js
+++ FrontEnd/assets/js/admin.js
@@ -1,10 +1,13 @@
 let token = localStorage.getItem('token')
 let adminBar = document.getElementById('admin-bar')
 let modal = null
+let portfolio = document.getElementById('portfolio')
 
 if (token) {
-    adminBar.innerHTML = `<i class="fa-regular fa-pen-to-square"></i><p class="mode-edition">Mode edition</p>`
+    adminBar.innerHTML = `<i class="fa-regular fa-pen-to-square"></i><p class="p-mode-edition">Mode edition</p>`
+    portfolio.innerHTML = `<p class="projets-mode-edition">Mes projets</p><i class="fa-regular fa-pen-to-square"></i><a href="#modal" class="p-mode-edition">modifier</a>`
 }
+
 
 const modeEdition = function(e) {
     e.preventDefault()
@@ -14,15 +17,23 @@ const modeEdition = function(e) {
     target.setAttribute('aria-modal', 'true')
     modal = target
     modal.addEventListener('click', closeModal)
+    modal.querySelector('js-modal-close').addEventListener('click', closeModal)
+    modal.querySelector('js-modal-stop').addEventListener('click', stopPropagation)
 }
+
+document.querySelector('.p-mode-edition').addEventListener('click', modeEdition)
+
+// modeEdition()
 
 const closeModal = function (e) {
     if (modal === null) return
     e.preventDefault()
-    modal.style.display = none
+    modal.style.display = "none"
     modal.setAttribute('aria-hidden', 'true')
     modal.removeAttribute('aria-modal')
     modal.removeEventListener('click', closeModal)
+    modal.querySelector('js-modal-close').removeEventListener('click', closeModal)
+    modal.querySelector('js-modal-stop').removeEventListener('click', stopPropagation)
     modal = null
 }
 
@@ -32,13 +43,13 @@ const stopPropagation = function (e) {
 
 document.querySelectorAll('admin-bar').forEach(a => {
     a.addEventListener('click', modeEdition)
-        if (e.key === "Escape" || e.key === "Esc") {
-            closeModal(e)
-        }
+        
 })
 
 window.addEventListener('keydown', function(e) {
-
+    if (e.key === "Escape" || e.key === "Esc") {
+        closeModal(e)
+    }
 })
 
 // let modal = document.getElementById('modal')
